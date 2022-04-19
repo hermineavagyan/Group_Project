197fe2e015f842c531fb509d3fe5bed33577b781
Hermine <herminemavagyan@gmail.com>
2022-04-19 15:18:42 -0700

    confirm password uncommented

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -27,26 +27,26 @@ const UserSchema = new mongoose.Schema({
     }
 },{timestamps:true})
 
-// UserSchema.virtual("confirmPassword")
-// .get(()=>this._confirmPassword)
-// .set((value)=>this._confirmPassword = value)
+UserSchema.virtual("confirmPassword")
+.get(()=>this._confirmPassword)
+.set((value)=>this._confirmPassword = value)
 
-// UserSchema.pre("validate", function(next){
-// if(this.password !== this.confirmPassword){
-// this.invalidate("confirmPassword", "Passwords must match!!!")
-// console.log("Passwords don't match!")
-// }
-// next()
-// })
-// UserSchema.pre("save", function(next){
-// console.log("in pre save");
-//     //hashing the password before it's saved to the db
-//     bcrypt.hash(this.password, 10)
-//         .then((hashedPassword)=>{
-//             this.password = hashedPassword;
-//             next();
-//         })
-//})
+UserSchema.pre("validate", function(next){
+if(this.password !== this.confirmPassword){
+this.invalidate("confirmPassword", "Passwords must match!!!")
+console.log("Passwords don't match!")
+}
+next()
+})
+UserSchema.pre("save", function(next){
+console.log("in pre save");
+    //hashing the password before it's saved to the db
+    bcrypt.hash(this.password, 10)
+        .then((hashedPassword)=>{
+            this.password = hashedPassword;
+            next();
+        })
+})
 
 const User = mongoose.model('User', UserSchema)
 
