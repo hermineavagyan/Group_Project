697a3d613d2c9caafaaea932d5dedb95ddd620c1
John Doe <95458294+Danbowman1@users.noreply.github.com>
2022-04-21 21:39:37 -0400

    displayOne price fixed

--- client/src/components/DisplayOne.js
+++ client/src/components/DisplayOne.js
@@ -8,20 +8,29 @@ const DisplayOne = () => {
 
     const {id} = useParams()
     const [product, setProduct] = useState({})
+    const [productPrice, setProductPrice] = useState('')
 
 
     useEffect(()=> {
         const itemGetter = async () => {
             try {
-                const res = await axios.get(`http://localhost:8000/api/items/${id}`)
-                console.log(res.data)
+                const res = await axios.get(`http://localhost:8000/products/one/${id}`)
+                
                 setProduct(res.data)
+                const price = await axios.get(`http://localhost:8000/prices/one/${id}`)
+                const str = price.data.data[0].unit_amount_decimal
+                var resStr = str.substring(0,str.length-2)+"."+str.substring(str.length-2);
+                // console.log(price.data.data[0].unit_amount_decimal)
+                
+                setProductPrice(resStr)
             } catch (err) {
                 console.log(err)
             }
         }
         itemGetter()
     }, [id])
+
+
 
     const paperStyle = {
         padding: '90px 40px',
@@ -39,16 +48,16 @@ const DisplayOne = () => {
             <Paper elevation={10} style={paperStyle}>
                 <Box style={{display: 'flex', flexDirection: 'column',minWidth: "400px"}}>
                         <h2>
-                        {product.item}
+                        {product.name}
                         </h2>
                     <CardMedia 
                     style={{}}
                     component='img'
-                    image={product.image}
+                    image={product.images}
                     />  
                 </Box>
                 <Box style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly'}}>
-                    <Typography>${product.price}</Typography>
+                    <Typography>$ {productPrice.toLocaleString()}</Typography>
                     <Typography>{product.description}</Typography>
                     <Button style={{width:'160px'}} variant="contained">Add to Cart</Button>
                 </Box>
