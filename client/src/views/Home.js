181ebaa6c87cfb159385917105506e555cc80fa2
KeithLion <nortonkiara6@gmail.com>
2022-04-21 23:32:21 -0700

    added more styling to home.js

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import {Button, Card, Box, CardHeader, CardContent} from '@material-ui/core'
+import {Button, Card, Box, CardHeader, CardContent, Grid, Grow} from '@material-ui/core'
+import NavBar from "../components/NavBar";
+import { textAlign } from "@mui/system";
 
 
 const Home = () => {
 
     const [itemList, setItemList] = useState([]);
+    const [products, setProducts] = useState({})
     const [user, setUser] = useState({});
 
     const navigate = useNavigate();
@@ -16,13 +19,15 @@ const Home = () => {
         display: 'block',
         width: '30vw',
         transitionDuration: '0.3s',
-        height: '45vw'
+        height: '40vw',
+        marginTop: '20px',
+        marginLeft:'18px'
     }
 
     useEffect(() => {
        const getAllItems = async (e)=>{
            try {
-            const res = await axios.get("http://localhost:8000/api/items")
+            const res = await axios.get("http://localhost:8000/products/all")
             
                 console.log(res);
                 console.log(res.data);
@@ -92,12 +97,28 @@ const Home = () => {
                     console.log(err);
                 };
     };
+
+    // const Item = (({theme})=>({
+    //     textAlign: 'center'
+    // }))
+
+
     return (
         <div>
+            <NavBar/>
+            <Box sx={{flexGrow:1}}>
+
+            <Grid container spacing={{sx:2, md:3}} columns={{xs:4, sm:8, md:12}}>
+
+{Array.from(Array(6)).map((_,index)=>(
+    <Grid  alignItems="center" item xs={2} sm={4} md={4} key={index}>
+
+
             {
+                
                 itemList.map((item, index) => (
-                    <Card elevation={10} style={cardStyle} 
-                        
+                    <Card elevation={10} style={cardStyle}
+                    sx={{ maxHeight: 275}}
                         key={item._id}>
                         
                         <Link to={`/item/${item._id}`}>{item.name}</Link>
@@ -105,14 +126,24 @@ const Home = () => {
                         <CardContent>
                         <p>{item.description}</p>
                         <p>{item.price}</p>
+                        <img 
+                        style={{width:'50%', minWidth: '168px', margin: '-3px auto', display: 'block'}}
+                        src={item.images}
+                        alt='product'
+                        />
                         </CardContent>
                         <Button onClick={()=>deleteItem(item._id)} >Delete</Button>
                         <Link to={`/item/edit/${item._id}`}>Edit</Link>
                         <hr/>
                     </Card>
 
-                ))
-            }
+))
+}
+
+</Grid>
+                            ))}
+                            </Grid>
+                            </Box>
 
         </div>
     )
