5ca754b71c496f5648bc67e279f41c4b3701926a
Will Martindale <wmartindale@gmail.com>
2022-04-23 15:27:47 -0700

    initial stripe implementation including pay button to submit a credit card with PaymentIntent

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,21 @@ import './App.css';
 import DisplayOne from './components/DisplayOne';
 import Login from './components/Login';
 import Registration from './components/Registration';
+import CheckoutForm from './components/CheckoutForm'
 import Home from './views/Home';
 
 function App() {
   return (
     <BrowserRouter>
-
       <div className="App">
         <Routes>
           <Route element={<Login />} path='/' />
           <Route element={<Registration />} path='/registration' />
           <Route element={<Home />} path='/home' />
           <Route element={<DisplayOne />} path='/products/:id' />
+          <Route element={<CheckoutForm />} path="/checkout" />
         </Routes>
-      
       </div>
-
     </BrowserRouter>
     
   );
